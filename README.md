e13b18f720673ecf7217a2c9849227c863b77f8b
rhn014 <Roland.Hyden@RegionHalland.se>
2019-07-15 11:35:19 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
-# region-halland-meta-data-region-halland
-Meta-data för sajten
+# Funktioner för att visa rätt metadata
+
+## Hur man använder Region Hallands plugin "region-halland-meta-data-region-halland"
+
+Nedan följer instruktioner hur du kan använda pluginet "region-halland-meta-data-region-halland".
+
+
+## Användningsområde
+
+Denna plugin innehåller två funktioner för att ta fram rätt metadata för siten
+
+
+## Licensmodell
+
+Denna plugin använder licensmodell GPL-3.0. Du kan läsa mer om denna licensmodell på:
+```sh
+A) Gnu.org (https://www.gnu.org/licenses/gpl-3.0.html)
+B) Wikipedia (https://sv.wikipedia.org/wiki/GNU_General_Public_License)
+```
+
+
+## Installation och aktivering
+
+```sh
+A) Hämta pluginen via Git eller läs in det med Composer
+B) Installera Region Hallands plugin i Wordpress plugin folder
+C) Aktivera pluginet inifrån Wordpress admin
+```
+
+
+## Hämta hem pluginet via Git
+
+```sh
+git clone https://github.com/RegionHalland/region-halland-meta-data-region-halland.git
+```
+
+
+## Läs in pluginen via composer
+
+Dessa två delar behöver du lägga in i din composer-fil
+
+Repositories = var pluginen är lagrad, i detta fall på github
+
+```sh
+"repositories": [
+  {
+    "type": "vcs",
+    "url": "https://github.com/RegionHalland/region-halland-meta-data-region-halland.git"
+  },
+],
+```
+Require = anger vilken version av pluginen du vill använda, i detta fall version 1.0.0
+
+OBS! Justera så att du hämtar aktuell version.
+
+```sh
+"require": {
+  "regionhalland/region-halland-acf-page-i-frame": "1.0.0"
+},
+```
+
+
+## Visa "meta description" i "head" via "Blade"
+
+OBS! Default antal ord är 25, men detta kan man själv välja genom att ange en parameter i anropet. Finns ingen parameter angiven retuneras 25 ord.
+
+```sh
+<meta name="description" content="{{ get_region_halland_meta_data_region_halland_description() }}" />
+```
+```sh
+<meta name="description" content="{{ get_region_halland_meta_data_region_halland_description(15) }}" />
+```
+
+## Visa "title" i "head" via "Blade"
+
+```sh
+<title>{{ get_region_halland_meta_data_region_halland_title() }}</title>
+```
+
+
+## Versionhistorik
+
+### 1.0.0
+- Första version
---
+++ region_halland_meta_data_region_halland.php
@@ -0,0 +1,127 @@
+<?php
+
+	/**
+	 * @package Region Halland Meta Data Region Halland
+	 */
+	/*
+	Plugin Name: Region Halland Meta Data Region Halland
+	Description: Front-end-plugin som returnerar metadata om applikationen
+	Version: 1.0.0
+	Author: Roland Hydén
+	License: GPL-3.0
+	Text Domain: regionhalland
+	*/
+
+	// Returnerar korrekt titel för en sida
+	function get_region_halland_meta_data_region_halland_title()
+	{
+		
+		// Aktuell sida
+		global $post;
+
+		// Hämta aktuell post_type
+		$myPostType = get_post_type();
+		
+		// Starta med en tom title-variabel
+		$myTitle = "";
+
+		// Hämta sitens namn från env-filen 
+		$mySiteName = ENV('SITE_NAME');
+
+		// Om det är en sida
+		if ($myPostType == 'page') {
+			
+			// Används sidans titel + sajtens namn
+			$myTitle = $post->post_title . " - " . $mySiteName;
+		
+		} else {
+
+			// Används namnet på post_type + sajtens namn
+			$myTitle = $myPostType . " - " . $mySiteName;
+
+		}
+
+		// Returnera sidans titel
+		return $myTitle;
+
+	}
+
+	// Returnerar korrekt titel för en sida
+	function get_region_halland_meta_data_region_halland_description($myWords = 25)
+	{
+		
+		// Aktuell sida
+		global $post;
+
+		// Starta med tom variabel fär beskrivning
+		$myDescription = "";
+
+		// Hämta default beskrivning från env-fil
+		$myDefaultDescription = ENV('SITE_DESCRIPTION');
+
+		// Hämta aktuell post_type
+		$myPostType = get_post_type();
+
+		// Gör olika beroende på post_type
+		switch ($myPostType) {
+			
+			// Om det är en sida
+			case 'page':
+				
+				// Hämta först ingress
+				$myIngess = get_field('name_1000109', $post->ID);
+				
+				// Om ingressen har ett värde, använd detta
+				if (strlen($myIngess) > 0) {
+					$myDescription = $myIngess;
+				
+				// Om ingressen är tom
+				} else {
+					
+					// Om the_content har ett värde, använd detta
+					if (strlen($post->post_content) > 0) {
+						
+						// Ta bort taggar 0 trimma det till 25 ord
+						$myContent = wp_strip_all_tags($post->post_content);
+						$myContent = wp_trim_words($myContent, $myWords, "");
+						
+						// Placera the_content i variabeln
+						$myDescription = $myContent;
+
+					} else {
+						
+						// Annars använd default beskrivning
+						$myDescription = $myDefaultDescription;
+					
+					}
+				}
+				break;
+			
+			// Annars använd default beskrivning	
+			default:
+				$myDescription = $myDefaultDescription;
+				break;
+		}
+
+		// Returnera sidans beskrivning
+		return $myDescription;
+
+	}
+
+	// Metod som anropas när pluginen aktiveras
+	function region_halland_meta_data_region_halland_activate() {
+		// Ingenting just nu...
+	}
+
+	// Metod som anropas när pluginen avaktiveras
+	function region_halland_meta_data_region_halland_deactivate() {
+		// Ingenting just nu...
+	}
+	
+	// Vilken metod som ska anropas när pluginen aktiveras
+	register_activation_hook( __FILE__, 'region_halland_meta_data_region_halland_activate');
+	
+	// Vilken metod som ska anropas när pluginen avaktiveras
+	register_deactivation_hook( __FILE__, 'region_halland_meta_data_region_halland_deactivate');
+
+?>
